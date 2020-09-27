1851a1125a91102f482cb08c8e90928da71960dd
LizzyModau26 <LizzyModau26@users.noreply.github.com>
2020-09-27 16:11:32 +0200

    new hotel ap folder, with homepage, about us, contact us  and gallery. edits still need to be implemented.

---
+++ my-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { GalleryComponent } from './gallery';
+import { ContactComponent } from './contact';
+import { AboutUsComponent } from './about-us';
+
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'about-us', component: AboutUsComponent }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
