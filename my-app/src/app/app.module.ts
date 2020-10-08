4f9c3ae37347575ceb80c2f774f0ae5a2fdd0312
banelekhumalo <67458707+banelekhumalo@users.noreply.github.com>
2020-10-08 12:16:04 +0200

    add to list code

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { GalleryComponent } from './gallery';
+import { ContactComponent } from './contact';
+import { AboutUsComponent } from './about-us';
+import { RegisterComponent } from './register';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GalleryComponent,
+    ContactComponent,
+    HomeComponent,
+    AboutUsComponent,
+    RegisterComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MDBBootstrapModule.forRoot(),
+    ReactiveFormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
